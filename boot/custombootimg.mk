91658d0319e27394c8718a09623863695d252bc4
Krzysztof <krzysztofjaniuk76@gmail.com>
2021-03-25 16:27:58 +0100

    Include also recovery

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -1,5 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
+$(INSTALLED_RECOVERYIMAGE_TARGET): \
+		$(MKBOOTIMG) \
+		$(recovery_ramdisk)
+	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
+	$(hide) $(MKBOOTIMG) \
+		--kernel $(PRODUCT_OUT)/kernel \
+		--ramdisk $(recovery_ramdisk) \
+		--cmdline "$(BOARD_KERNEL_CMDLINE)" \
+		--base $(BOARD_KERNEL_BASE) \
+		--pagesize $(BOARD_KERNEL_PAGESIZE) \
+		--dt $(PRODUCT_OUT)/dt.img \
+		$(BOARD_MKBOOTIMG_ARGS) \
+		--output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	$(call pretty,"Made recovery image: $@")
+
 $(INSTALLED_BOOTIMAGE_TARGET): \
 		$(MKBOOTIMG) \
 		$(INTERNAL_BOOTIMAGE_FILES) \
