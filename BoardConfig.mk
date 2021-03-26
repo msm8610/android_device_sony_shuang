ecf4947e9ea8330f63f5ebd1a549757b6a506a2d
Krzysztof <krzysztofjaniuk76@gmail.com>
2021-03-26 11:21:06 +0100

    Restore previous audio_policy.conf
    This reverts commit 79be797ada6f57efff9d175b3b487bf02aa5ca34.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,6 @@ DEVICE_PATH := device/sony/shuang
 
 TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
 
-# It's temporary solution... I need to fix mess with recovery.img
-BUILD_BROKEN_DUP_RULES := true
-
 # inherit from the proprietary version
 include vendor/sony/shuang/BoardConfigVendor.mk
 
