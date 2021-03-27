3da5fd7652fb63ccf87a45d7858bd3c5a3476495
Krzysztof <krzysztofjaniuk76@gmail.com>
2021-03-27 09:21:11 +0100

    Don't use functions for DualSim
    It makes error from /systen/core/init/util.h idk why

--- init/init_shuang.cpp
+++ init/init_shuang.cpp
@@ -34,11 +34,11 @@
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-#include <cutils/properties.h>
-#include <init/util.h>
+//#include <cutils/properties.h>
+//#include <init/util.h>
 
 #include "property_service.h"
-#include "util.h"
+//#include "util.h"
 #include "vendor_init.h"
 
 #include <android-base/logging.h>
@@ -50,7 +50,8 @@
 enum { D2004, D2005, D2104, D2105, D2114 };
 
 using android::base::GetProperty;
-using android::init::import_kernel_cmdline;
+//using android::init::import_kernel_cmdline;
+using android::init::property_set;
 
 void property_override(char const prop[], char const value[])
 {
